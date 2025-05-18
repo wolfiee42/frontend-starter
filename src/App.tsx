04115d644fb38f5021f5bf1bae1ef073cae4a1e0
wolfiee42 <saifalislam2022@gmail.com>
2025-05-18 11:02:54 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl text-zinc-800 font-[700] flex justify-center items-center h-screen">
+      Welcome to Frontend React + Typescript Starter.
+    </h1>
+  );
+}
+
+export default App;
